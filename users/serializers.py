835d763aafd629c750d2c5d1c2cf4d57c9654f61
Aditiadi759@gmail.com <aditiadi759@gmail.com>
2026-03-15 22:57:42 +0530

    Updated login API implementation

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model 
+from django.contrib.auth import authenticate
 
 User = get_user_model()
 
@@ -21,7 +22,21 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(username=validated_data['username'], email=validated_data['email'], password=validated_data['password'])         
         return user    
 
-
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only = True)
+    
+    def validate(self, data):
+        user = authenticate(
+            username = data['username'],
+            password = data['password']
+        )
+        
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+        
+        data['user'] = user
+        return data
 
 
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 from .serializers import RegisterSerializer
+from .serializers import LoginSerializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    
+class LoginView(APIView):
+    def post(self, request):
+        Serializer = LoginSerializer(data = request.data)
+
+        if Serializer.is_valid():
+            return Response({"message":"Login Successful"}, status = status.HTTP_200_OK)
+        
+        return Response(Serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
