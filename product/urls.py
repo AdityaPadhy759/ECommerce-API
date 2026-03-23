ae05855cad9776ed588d3bb595acee1f97352bcb
Aditiadi759@gmail.com <aditiadi759@gmail.com>
2026-03-23 21:29:34 +0530

    search bar and filter products by category and price

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProductListAPI,ProductDetailsAPI, ProductCategorySerializer, ProductImageAPI, ProductStockAPI
 
 urlpatterns = [
-    path('products/', ProductListAPI.as_view()),
+    path('products/', ProductListAPI.as_view(), name = 'product-list'),
     path('products/<int:id>/', ProductDetailsAPI.as_view(), name='product-detail'),
     path('categories/', ProductDetailsAPI.as_view(), name = 'categpry-list'),
     path('products/<int:id>/images/', ProductImageAPI.as_view(), name = 'product-image'),
--- product/views.py
+++ product/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Product, Category, ProductImage
 from rest_framework import generics
 from .serializers import ProductListSerializer, ProductCategorySerializer, ProductImageSerializer, ProductStockSerializer
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
 # ProductList API
@@ -37,3 +38,33 @@ class ProductStockAPI(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductStockSerializer.lookup_field = 'id'
     
+#Search & Filter
+class ProductListAPI(generics.ListAPIView):
+    serializer_class = ProductListSerializer
+    
+    def get_queryset(self):
+        queryset = Product.objects.all()
+    
+        #search
+        search = self.request.query_params.get('search')
+        if search:
+            queryset = queryset.filter(name__icontains = search)
+        
+        #filter by category
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name__icontains=category)
+        
+        #filter by price
+        min_price = self.request.query_params.get('min_price')
+        max_price = self.request.query_params.get('max_price')
+        
+        if min_price:
+            queryset = queryset.filter(price__gte = min_price)
+        
+        if max_price:
+            queryset = queryset.filter(price__lte = max_price)
+            
+            return queryset
+        
+        
